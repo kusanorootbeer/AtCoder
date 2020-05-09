5fe3c23629f3a37cf10962452c0e323259906fcc
kusano <kusano@ai.cs.kobe-u.ac.jp>
2020-05-10 07:42:42 +0900

    fix shortpath

--- components/search/shortest_path.py
+++ components/search/shortest_path.py
@@ -40,10 +40,11 @@ def main():
         dijkstraを使いたいときは, 引数limitが必要になる
     numpy arrayじゃなくてcsr_matrixの方が高速らしい
     '''
+    N = 5
     cost = [1, 2, 1, 3, 3]
     starts = [0, 0, 1, 2, 2]
     nexts = [1, 2, 3, 0, 3]
-    csr = csr_matrix((cost, (starts, nexts)), shape=(5, 5))
+    csr = csr_matrix((cost, (starts, nexts)), shape=(N, N))
     print(csr)
 
 
